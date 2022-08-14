e163c216cab2347b74bbc7da870988ef7df8adb0
Melnikov <artem.melnikov.ara@gmail.com>
2022-08-14 08:27:16 +0300

    HomeWork 2 Exercise 3

---
+++ HomeWork-2_Exercise3/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 15:  Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+string answer = "нет";
+
+if (0 < number && number< 8)
+{
+    if (number == 6 || number == 7)
+    {
+        answer = "да";
+    }
+    Console.WriteLine(answer);
+}
+else
+{
+    Console.WriteLine("Дня недели с таким номером не существует.");
+}
