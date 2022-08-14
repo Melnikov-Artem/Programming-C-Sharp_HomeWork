8e0d159d006a9bfe2218711d7342b9d78a23c7a9
Melnikov <artem.melnikov.ara@gmail.com>
2022-08-14 07:45:14 +0300

    HomeWork 2 Exercise 2

---
+++ HomeWork-2_Exercise2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 13:  Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет. 
+(Строки и циклы в решении использовать нельзя!)
+645 -> 6
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number / 100 % 10;
+
+if (result>0)
+{
+    Console.WriteLine($"Третий разряд числа {number} равен = {result}");
+}
+else
+{
+    if (number/1000 > 0)
+    {
+        Console.WriteLine($"Третий разряд числа {number} равен = 0");
+    }
+    else
+    {
+        Console.WriteLine($"У числа {number} третьего разряда нет");
+    }
+}
