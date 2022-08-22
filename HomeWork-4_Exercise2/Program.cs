1e3072567cc1be95b1f60af8d30812a025139318
Melnikov <artem.melnikov.ara@gmail.com>
2022-08-22 21:46:45 +0300

    HomeWork 4 Exercise 2

---
+++ HomeWork-4_Exercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 27: Напишите функцию и запустите ее, 
+которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Clear();
+
+int EnterNumber()
+{
+    Console.WriteLine("Введите число:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumNumber(int num)
+{
+    int result = 0;
+    int check = num;
+    while (check != 0)
+    {
+        result = result + check % 10;
+        check = check / 10;
+    }
+    Console.WriteLine($"Сумма цифр в числе {num} = {result}");
+}
+
+int number = EnterNumber();
+SumNumber(number);
